ac03445b7b8e1d2d26030b48ebf568338da09077
Cyri0 <nemestamas94@gmail.com>
2025-03-06 11:06:00 +0100

    card pick added

--- blackjack.js
+++ blackjack.js
@@ -3,11 +3,19 @@ const BASE_URL = "https://deckofcardsapi.com/api/deck"
 const newDeckUrl = BASE_URL + "/new/shuffle/?deck_count=3"
 let deckId = undefined
 
+const newDeckBtn = document.getElementById("newDeckBtn")
+const hitBtn = document.getElementById("hitBtn")
+
+hitBtn.disabled = true
+
 function getNewDeck(){
     fetch(newDeckUrl)
     .then(response => response.json())
     .then(deckData => {
         deckId = deckData.deck_id
+
+        newDeckBtn.disabled = true
+        hitBtn.disabled = false
     })
 }
 
@@ -16,5 +24,9 @@ function pickCard(){
         console.error("Nincs paklink!");
         return
     }
-    fetch(BASE_URL + "/deckId/draw/?count=2")
+    fetch(BASE_URL + `/${deckId}/draw/?count=2`)
+    .then(response => response.json())
+    .then(cards => {
+            console.log(cards);
+    })
 }
